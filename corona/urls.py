1279e0993c4065d5cde7bc8baae8ad69cd83c443
shymii <s.szymon1127@gmail.com>
2020-10-20 12:01:09 +0200

    added covid sites

--- corona/urls.py
+++ corona/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('covid/', views.covid_view, name="covid"),
+    path('covid/global/', views.global_covid_view, name="globalcovid"),
+    path('covid/compare/', views.compare_covid_view, name="comparecovid"),
+    path('covid/country/', views.country_covid_view, name="countrycovid")
 ]
--- corona/views.py
+++ corona/views.py
@@ -5,3 +5,18 @@ def covid_view(request):
     template = 'covid/covid.html'
     context = {}
     return render(request, template, context)
+
+def global_covid_view(request):
+    template = 'covid/globalcovid.html'
+    context = {}
+    return render(request, template, context)
+
+def compare_covid_view(request):
+    template = 'covid/comparecovid.html'
+    context = {}
+    return render(request, template, context)
+
+def country_covid_view(request):
+    template = 'covid/countrycovid.html'
+    context = {}
+    return render(request, template, context)
