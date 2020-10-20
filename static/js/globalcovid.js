2ca7c89e74127843b17fc70c7ab46667b41195a2
raczu <61229912+raczu@users.noreply.github.com>
2020-10-20 23:59:57 +0200

    radek info
    wez to wrzucaj w pliku js'a dziemki

---
+++ static/js/globalcovid.js
@@ -0,0 +1,92 @@
+function commaSeparateNumber(val){
+    while (/(\d+)(\d{3})/.test(val.toString())){
+    val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+    }
+    return val;
+}
+fetch(`https://covid19-api.org/api/timeline`)
+        .then(res => res.json())
+        .then(data => {
+        var cases = []
+        var labels = []
+        var deaths = []
+        var recovered = []
+        var daily = []
+        let n = 0;
+        data.forEach(element => {
+            cases.unshift(data[n].total_cases);
+            let masno = data[n].last_update;
+            let update = masno.replace("T", " ")
+            labels.unshift(update)
+            deaths.unshift(data[n].total_deaths)
+            recovered.unshift(data[n].total_recovered)
+            n++;
+        });
+        n--;
+        document.getElementById("dailyRes").innerHTML = `Dzisiejsza liczba przypadków: ${commaSeparateNumber(cases[n] - cases[n-1])}`;
+        document.getElementById("casesRes").innerHTML = `Łączna liczba przypadków: ${commaSeparateNumber(cases[n])}`;
+        document.getElementById("deathRes").innerHTML = `Śmierci łącznie: ${commaSeparateNumber(deaths[n])}`;
+        document.getElementById("recoveredRes").innerHTML = `Wyzdrowiałych łącznie: ${commaSeparateNumber(recovered[n])}`;
+        for (n ; n > 0 ; n--) {
+            let val = cases[n + 1] - cases[n];
+            if (val < 0) {
+                val = -(val);
+            }
+            daily.unshift(val);
+        }
+        var myChart = document.getElementById("myChart").getContext('2d');
+        var dailyChart = document.getElementById("dailyChart").getContext('2d');
+        var deathChart = document.getElementById("deathChart").getContext('2d');
+        var chart = new Chart(myChart, {
+            type:'line',
+            data:{
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Potwierdzone przypadki",
+                        data: cases,
+                        backgroundColor: '#CE0217',
+                        minBarLength: 100
+                    }
+                ]
+            },
+            options:{}
+        })
+        var dailyCh = new Chart(dailyChart, {
+            type:'line',
+            data:{
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Dzienne przypadki",
+                        data: daily,
+                        backgroundColor: '#3585CA',
+                        minBarLength: 100
+                    }
+                ]
+            },
+            options:{}
+        })
+        var deathCh = new Chart(deathChart, {
+            type:'line',
+            data:{
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Ozdrowione przypadki",
+                        data: recovered,
+                        backgroundColor: '#32cd32',
+                        minBarLength: 100
+                    },
+
+                    {
+                        label: "Śmierci",
+                        data: deaths,
+                        backgroundColor: '#696969',
+                        minBarLength: 100
+                    }
+                ]
+            },
+            options:{}
+        })
+    });
