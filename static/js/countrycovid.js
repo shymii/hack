ff1d4e8e81607d28e65631a323dc4caeae87ea22
hunchoradek <snejkbeast@gmail.com>
2020-10-21 15:45:29 +0200

    started working on single country covid stats

---
+++ static/js/countrycovid.js
@@ -0,0 +1,68 @@
+function masno(){
+    let country = document.getElementById("country").value;
+    fetch(`https://covid19-api.org/api/timeline/${country}`)
+            .then(res => res.json())
+            .then(data => {
+            var cases = []
+            var labels = []
+            var deaths = []
+            var recovered = []
+            var daily = []
+            let n = 0;
+            console.log(data);
+            data.forEach(element => {
+                cases.unshift(data[n].cases);
+                let masno = data[n].last_update;
+                let update = masno.replace("T", " ")
+                labels.unshift(update)
+                deaths.unshift(data[n].deaths)
+                recovered.unshift(data[n].recovered)
+                n++;
+            });
+            for (n ; n > 0 ; n--) {
+                let val = cases[n + 1] - cases[n];
+                if (val < 0) {
+                    val = -(val);
+                }
+                daily.unshift(val);
+            }
+            var myChart = document.getElementById("myChart").getContext('2d')
+
+            var chart = new Chart(myChart, {
+                type:'line',
+                data:{
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: "Potwierdzone przypadki",
+                            data: cases,
+                            backgroundColor: '#8b0000',
+                            minBarLength: 100
+                        },
+
+                        {
+                            label: "Liczba zgonów",
+                            data: deaths,
+                            backgroundColor: '#696969',
+                            minBarLength: 100
+                        },
+                        
+                        {
+                            label: "Wyleczone przypadki",
+                            data: recovered,
+                            backgroundColor: '#32cd32',
+                            minBarLength: 100
+                        },
+
+                        {
+                            label: "Dzienna liczba przypadków",
+                            data: daily,
+                            backgroundColor: '#7188e6',
+                            minBarLength: 100
+                        }
+                    ]
+                },
+                options:{}
+            })
+        });
+}
---
+++ static/js/dist/countrycovid.dev.js
@@ -0,0 +1,65 @@
+"use strict";
+
+function masno() {
+  var country = document.getElementById("country").value;
+  fetch("https://covid19-api.org/api/timeline/".concat(country)).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    var cases = [];
+    var labels = [];
+    var deaths = [];
+    var recovered = [];
+    var daily = [];
+    var n = 0;
+    console.log(data);
+    data.forEach(function (element) {
+      cases.unshift(data[n].cases);
+      var masno = data[n].last_update;
+      var update = masno.replace("T", " ");
+      labels.unshift(update);
+      deaths.unshift(data[n].deaths);
+      recovered.unshift(data[n].recovered);
+      n++;
+    });
+
+    for (n; n > 0; n--) {
+      var val = cases[n + 1] - cases[n];
+
+      if (val < 0) {
+        val = -val;
+      }
+
+      daily.unshift(val);
+    }
+
+    var myChart = document.getElementById("myChart").getContext('2d');
+    var chart = new Chart(myChart, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Potwierdzone przypadki",
+          data: cases,
+          backgroundColor: '#8b0000',
+          minBarLength: 100
+        }, {
+          label: "Liczba zgonów",
+          data: deaths,
+          backgroundColor: '#696969',
+          minBarLength: 100
+        }, {
+          label: "Wyleczone przypadki",
+          data: recovered,
+          backgroundColor: '#32cd32',
+          minBarLength: 100
+        }, {
+          label: "Dzienna liczba przypadków",
+          data: daily,
+          backgroundColor: '#7188e6',
+          minBarLength: 100
+        }]
+      },
+      options: {}
+    });
+  });
+}
