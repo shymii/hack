195e67b28bd5d928ef519cbcf8460e47f4da3b6b
raczu <61229912+raczu@users.noreply.github.com>
2020-10-23 00:26:15 +0200

    added caloric demand calculator

--- physical_health/views.py
+++ physical_health/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from users.models import user_survey
+import datetime
 
 # Create your views here.
 def physical_health_view(request):
+    if request.user.is_authenticated:
+        survey_result = user_survey.objects.filter(user = request.user.user_profile).order_by('-survey_date')[0]
+        year = request.user.user_profile.birth_date.year
+        now = datetime.datetime.now()
+        year_now = now.year
+        birth_date = year_now - year
+        gender = request.user.user_profile.sex
+        context = {'survey_result' : survey_result, 'birth_date' : birth_date, 'gender' : gender}
+    else:
+        context = {}
     template = 'physical_health/physical_health.html'
-    context = {}
     return render(request, template, context)
---
+++ static/js/caloricdemand.js
@@ -0,0 +1,120 @@
+const btn = document.querySelector('#btn');
+
+btn.addEventListener("click", e => {
+    e.preventDefault();
+    const gender = document.getElementsByName('gender');
+    const weight = document.querySelector('#weight');
+    const height = document.querySelector('#height');
+    const age = document.querySelector('#age');
+    const life_style = document.querySelector('#life-style');
+    const somatype = document.querySelector('#somatype');
+    const purpose = document.getElementsByName('purpose');
+    
+    var gender_value;
+    gender.forEach(sex => {
+        if (sex.checked) {
+            gender_value = sex.value;
+        }
+    });
+    let weight_value = parseFloat(weight.value);
+    let height_value = parseInt(height.value);
+    let age_value = parseInt(age.value);
+    let life_style_value = parseFloat(life_style.value);
+    let somatype_value = somatype.value;
+    var purpose_value;
+    purpose.forEach(element => {
+        if (element.checked) {
+            purpose_value = element.value;
+        }
+    });
+
+    let validation = 0;
+    let sum_;
+    let daily_caloric_demand;
+    if (gender_value) {
+        gender.forEach(element => {
+            element.classList.remove('form-div-input-wrong');
+        });
+        validation++;
+    } else {
+        gender.forEach(element => {
+            element.classList.add('form-div-input-wrong');
+        });
+    }
+
+    if (weight_value > 0) {
+        weight.classList.remove('form-div-input-wrong');
+        validation++;
+    } else {
+        weight.classList.add('form-div-input-wrong');
+    }
+
+    if (height_value > 0) {
+        height.classList.remove('form-div-input-wrong');
+        validation++;
+    } else {
+        height.classList.add('form-div-input-wrong');
+    }
+
+    if (age_value > 0) {
+        age.classList.remove('form-div-input-wrong');
+        validation++;
+    } else {
+        age.classList.add('form-div-input-wrong');
+    }
+
+    if (validation === 4) {
+        if (gender_value === 'mezczyzna') {
+            sum_ = 66.5 + (13.7 * weight_value) + (5 * height_value) - (6.8 * age_value);
+            daily_caloric_demand = sum_ * life_style_value;
+            if ((somatype_value != 'nie-znam') && purpose_value) {
+                if (somatype_value === 'Ektomorfik') {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.2;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.1;
+                    }
+                } else if (somatype_value === 'Mezomorfik') {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.15;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.15;
+                    }
+                } else {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.1;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.2;
+                    }
+                }
+            }
+        } else {
+            sum_ = 665 + (9.6 * weight_value) + (1.85 * height_value) - (4.7 * age_value);
+            daily_caloric_demand = sum_ * life_style_value;
+            if ((somatype_value != 'nie-znam') && purpose_value) {
+                if (somatype_value === 'Ektomorfik') {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.2;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.1;
+                    }
+                } else if (somatype_value === 'Mezomorfik') {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.15;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.15;
+                    }
+                } else {
+                    if (purpose_value === 'masa') {
+                        daily_caloric_demand += daily_caloric_demand*0.1;
+                    } else {
+                        daily_caloric_demand -= daily_caloric_demand*0.2;
+                    }
+                }
+            }     
+        }
+        daily_caloric_demand = Math.ceil(daily_caloric_demand);
+        console.log(daily_caloric_demand);
+    }
+
+});
