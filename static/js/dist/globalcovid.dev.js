1f82cfc73b183c364e2db73deb63f4f21fc4e957
hunchoradek <snejkbeast@gmail.com>
2020-10-24 00:36:34 +0200

    commas

--- static/js/dist/globalcovid.dev.js
+++ static/js/dist/globalcovid.dev.js
@@ -2,7 +2,7 @@
 
 function commaSeparateNumber(val) {
   while (/(\d+)(\d{3})/.test(val.toString())) {
-    val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ' ' + '$2');
+    val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
   }
 
   return val;
