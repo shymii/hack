b48a63d91f24d8bbc082f9931abe24c2b6800cf9
raczu <61229912+raczu@users.noreply.github.com>
2020-10-24 23:31:31 +0200

    google map + destinations

--- static/js/dist/map.dev.js
+++ static/js/dist/map.dev.js
@@ -1,16 +1,56 @@
 "use strict";
 
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB-g39QK3WxgK7Pk1E_j41_4JL0_w2OnLc&callback=initMap&libraries=places';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB-g39QK3WxgK7Pk1E_j41_4JL0_w2OnLc&callback=initMap&libraries=places&language=pl&region=PL';
 script.defer = true;
 var map;
+var markers = [];
+var place_;
 
 var createMarker = function createMarker(place, ID) {
+  var image = {
+    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+  };
   var marker = new google.maps.Marker({
     map: map,
     animation: google.maps.Animation.DROP,
     position: place.geometry.location,
-    title: place.name
+    title: place.name,
+    icon: image
+  });
+  markers.push(marker);
+  var distance = new google.maps.DistanceMatrixService();
+  document.querySelector('#output').innerHTML = '';
+  distance.getDistanceMatrix({
+    origins: [place_],
+    destinations: [place.geometry.location],
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    drivingOptions: {
+      departureTime: new Date(Date.now() + 600000),
+      trafficModel: 'bestguess'
+    }
+  }, function (response, status) {
+    if (status === 'OK') {
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
+      var output_div = document.querySelector('#output');
+      var template_string = "";
+
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+
+        for (var j = 0; j < destinations.length; j++) {
+          if (results[j].duration.value < 900) {
+            template_string += "<p><span>".concat(origins[i], "</span><span>").concat(place.name, "</span><span>").concat(results[j].distance.text, "</span><span>").concat(results[j].duration.text, "</span></p>");
+          }
+        }
+      }
+
+      output_div.innerHTML += template_string;
+    } else {
+      output_div.innerHTML = 'Nie znaleziono obiektów w twojej okolicy';
+    }
   });
   marker.addListener('click', function () {
     var request = {
@@ -34,24 +74,74 @@ var callback = function callback(results, status) {
   }
 };
 
+var remove_markers = function remove_markers() {
+  markers.forEach(function (marker) {
+    marker.setMap(null);
+  });
+};
+
 window.initMap = function () {
+  var input = document.querySelector('#search');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
   var canvas = document.getElementById('map');
-  var location = new google.maps.LatLng(50.3013283, 18.5795769);
+  var location;
+  var default_location = new google.maps.LatLng(50.2942602, 18.6635169);
   var options = {
-    center: location,
-    zoom: 10
+    center: default_location,
+    zoom: 13
   };
   map = new google.maps.Map(canvas, options);
-  var request = {
-    location: location,
-    radius: '5000',
-    type: ['gym']
-  };
+  service = new google.maps.places.PlacesService(map);
+
+  if (!navigator.geometry) {
+    var request = {
+      location: default_location,
+      radius: '2000',
+      type: ['gym']
+    };
+    place_ = default_location;
+    service.nearbySearch(request, callback);
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      place_ = location;
+      map.setCenter(location);
+      var request = {
+        location: location,
+        radius: '2000',
+        type: ['gym']
+      };
+      document.querySelector('#xdd').innerHTML = 'Znaleziono lokalizacje';
+      remove_markers();
+      service.nearbySearch(request, callback);
+    });
+  }
+
   infowindow = new google.maps.InfoWindow({
     maxWidth: 250
   });
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  autocomplete.addListener('place_changed', function () {
+    var place = autocomplete.getPlace();
+
+    if (place.geometry) {
+      remove_markers();
+      location = place.geometry.location;
+      place_ = location;
+      map.setCenter(location);
+      var _request = {
+        location: location,
+        radius: '2000',
+        type: ['gym']
+      };
+      service.nearbySearch(_request, callback);
+    }
+  });
 };
 
 document.head.appendChild(script);
