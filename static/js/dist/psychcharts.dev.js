84a2dc2e287ce8afe087607a8494727acc354c24
hunchoradek <snejkbeast@gmail.com>
2020-10-24 16:11:17 +0200

    next chart

--- static/js/dist/psychcharts.dev.js
+++ static/js/dist/psychcharts.dev.js
@@ -7,42 +7,71 @@ var australiaOb = [61.74, 60.00, 55.16, 49.23, 46.20, 43.57];
 var southamericaOb = [73.57, 76.44, 74.83, 75.87, 76.70, 75.50];
 var northamericaOb = [75.57, 77.00, 77.33, 74.47, 68.72, 68.22];
 var europeOb = [97.42, 111.49, 107.35, 109.10, 96.87, 88.61];
+var africaCal = 2624;
+var asiaCal = 2779;
+var australiaCal = 3276;
+var southamericaCal = 3027;
+var northamericaCal = 3663;
+var europeCal = 3367;
 var myPsychChart = document.getElementById("myPsychChart").getContext('2d');
+var barChart = document.getElementById("myBarChart").getContext('2d');
 var Pchart = new Chart(myPsychChart, {
   type: 'line',
   data: {
     labels: labels,
     datasets: [{
-      label: "Afryka",
+      label: "Afryka,",
       data: africaOb,
-      backgroundColor: '#FFEA80',
+      backgroundColor: 'rgba(255, 99, 132, 0.4)',
       minBarLength: 100
     }, {
       label: "Azja",
       data: asiaOb,
-      backgroundColor: '#FFAA80',
+      backgroundColor: 'rgba(54, 162, 235, 0.4)',
       minBarLength: 100
     }, {
       label: "Australia",
       data: australiaOb,
-      backgroundColor: '#B3B300',
+      backgroundColor: 'rgba(255, 206, 86, 0.4)',
       minBarLength: 100
     }, {
       label: "Ameryka Południowa",
       data: southamericaOb,
-      backgroundColor: '#4DFF88',
+      backgroundColor: 'rgba(75, 192, 192, 0.4)',
       minBarLength: 100
     }, {
       label: "Ameryka Północna",
       data: northamericaOb,
-      backgroundColor: '#E14DFF',
+      backgroundColor: 'rgba(153, 102, 255, 0.4)',
       minBarLength: 100
     }, {
       label: "Europa",
       data: europeOb,
-      backgroundColor: '#4D6AFF',
+      backgroundColor: 'rgba(255, 159, 64, 0.4)',
       minBarLength: 100
     }]
   },
   options: {}
+});
+var myBarChart = new Chart(barChart, {
+  type: 'bar',
+  data: {
+    labels: ['Afryka', 'Azja', 'Ameryka Południowa', 'Ameryka Północna', 'Australia', 'Europa'],
+    datasets: [{
+      label: 'liczba kcal',
+      data: [africaCal, asiaCal, southamericaCal, northamericaCal, australiaCal, europeCal],
+      backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+      borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
 });
--- static/js/psychcharts.js
+++ static/js/psychcharts.js
@@ -7,7 +7,15 @@ let southamericaOb = [73.57, 76.44, 74.83, 75.87, 76.70, 75.50];
 let northamericaOb = [75.57, 77.00, 77.33, 74.47, 68.72, 68.22];
 let europeOb = [97.42, 111.49, 107.35, 109.10, 96.87, 88.61];
 
+let africaCal = 2624;
+let asiaCal = 2779;
+let australiaCal = 3276;
+let southamericaCal = 3027;
+let northamericaCal = 3663;
+let europeCal = 3367;
+
 var myPsychChart = document.getElementById("myPsychChart").getContext('2d');
+var barChart = document.getElementById("myBarChart").getContext('2d');
 
 var Pchart = new Chart(myPsychChart, {
     type:'line',
@@ -15,39 +23,39 @@ var Pchart = new Chart(myPsychChart, {
         labels: labels,
         datasets: [
             {
-                label: "Afryka",
+                label: "Afryka,",
                 data: africaOb,
-                backgroundColor: '#FFEA80',
+                backgroundColor: 'rgba(255, 99, 132, 0.4)',
                 minBarLength: 100
             },
             {
                 label: "Azja",
                 data: asiaOb,
-                backgroundColor: '#FFAA80',
+                backgroundColor: 'rgba(54, 162, 235, 0.4)',
                 minBarLength: 100
             },
             {
                 label: "Australia",
                 data: australiaOb,
-                backgroundColor: '#B3B300',
+                backgroundColor: 'rgba(255, 206, 86, 0.4)',
                 minBarLength: 100
             },
             {
                 label: "Ameryka Południowa",
                 data: southamericaOb,
-                backgroundColor: '#4DFF88',
+                backgroundColor: 'rgba(75, 192, 192, 0.4)',
                 minBarLength: 100
             },
             {
                 label: "Ameryka Północna",
                 data: northamericaOb,
-                backgroundColor: '#E14DFF',
+                backgroundColor: 'rgba(153, 102, 255, 0.4)',
                 minBarLength: 100
             },
             {
                 label: "Europa",
                 data: europeOb,
-                backgroundColor: '#4D6AFF',
+                backgroundColor: 'rgba(255, 159, 64, 0.4)',
                 minBarLength: 100
             },
         ]
@@ -55,3 +63,39 @@ var Pchart = new Chart(myPsychChart, {
     options:{
     }
 })
+var myBarChart = new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: ['Afryka', 'Azja', 'Ameryka Południowa', 'Ameryka Północna', 'Australia', 'Europa'],
+        datasets: [{
+            label: 'liczba kcal',
+            data: [africaCal, asiaCal, southamericaCal, northamericaCal, australiaCal, europeCal],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
