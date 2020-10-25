a31cbfc5a64802559a28647e2d842facdb90ef13
shymii <s.szymon1127@gmail.com>
2020-10-25 15:46:28 +0100

    country charts fix

--- static/js/countrycovid-light.js
+++ static/js/countrycovid-light.js
@@ -108,72 +108,84 @@ countrybtn.addEventListener("click", (e) => {
             fetch(`https://covid19-api.org/api/prediction/${country}`)
             .then(res => res.json())
             .then(data => {
-                let cases = []
-                let labels = []
-                let daily = []
-                let idkidk = []
-                let n = 0;
-                data.forEach(element => {
-                    cases.push(data[n].cases);
-                    let masno = data[n].date;
-                    let update = "";
-                    for (let i = 5; i < 10 ; i++) {
-                        update += masno[i]
+                if(!data['error']){
+                    let a = document.getElementsByClassName('pred');
+                    for(let item of a){
+                        item.style.display = 'block'
                     }
-                    update = update.replace("-", ".");
-                    labels.push(update);
-                    idkidk.push(update);
-                    n++;
-                });
-                for (n ; n > 0 ; n--) {
-                    let val = cases[n - 1] - cases[n];
-                    if (val < 0) {
-                        val = -(val);
+                    let cases = []
+                    let labels = []
+                    let daily = []
+                    let idkidk = []
+                    let n = 0;
+                    data.forEach(element => {
+                        cases.push(data[n].cases);
+                        let masno = data[n].date;
+                        let update = "";
+                        for (let i = 5; i < 10 ; i++) {
+                            update += masno[i]
+                        }
+                        update = update.replace("-", ".");
+                        labels.push(update);
+                        idkidk.push(update);
+                        n++;
+                    });
+                    for (n ; n > 0 ; n--) {
+                        let val = cases[n - 1] - cases[n];
+                        if (val < 0) {
+                            val = -(val);
+                        }
+                        daily.push(val);
+                    };
+                    idkidk.shift();
+                    daily.shift();
+                    daily.reverse();
+                    document.getElementById("pred").innerHTML = '&nbsp;';
+                    document.getElementById("pred").innerHTML = '<canvas id="predChart"></canvas>';
+                    document.getElementById("dailypred").innerHTML = '&nbsp;';
+                    document.getElementById("dailypred").innerHTML = '<canvas id="dpredChart"></canvas>';
+                    let predChart = document.getElementById("predChart").getContext('2d');
+                    let dailypredChart = document.getElementById("dpredChart").getContext('2d');
+                    let pchart = new Chart(predChart, {
+                        type:'line',
+                        data:{
+                            labels: labels,
+                            datasets: [
+                                {
+                                    label: "Potwierdzone przypadki",
+                                    data: cases,
+                                    backgroundColor: 'rgba(206, 2, 23, 0.6)',
+                                    minBarLength: 100
+                                }
+                            ]
+                        },
+                        options:{
+                        }
+                    });
+                    let dpredCh = new Chart(dailypredChart, {
+                        type:'line',
+                        data:{
+                            labels: idkidk,
+                            datasets: [
+                                {
+                                    label: "Dzienne przypadki",
+                                    data: daily,
+                                    backgroundColor: 'rgba(53, 133, 202, 0.6)',
+                                    minBarLength: 100
+                                }
+                            ]
+                        },
+                        options:{
+                        }
+                    });
+                } else {
+                    let a = document.getElementsByClassName('pred');
+                    for(let item of a){
+                        item.style.display = 'none'
                     }
-                    daily.push(val);
-                };
-                idkidk.shift();
-                daily.shift();
-                daily.reverse();
-                document.getElementById("pred").innerHTML = '&nbsp;';
-                document.getElementById("pred").innerHTML = '<canvas id="predChart"></canvas>';
-                document.getElementById("dailypred").innerHTML = '&nbsp;';
-                document.getElementById("dailypred").innerHTML = '<canvas id="dpredChart"></canvas>';
-                let predChart = document.getElementById("predChart").getContext('2d');
-                let dailypredChart = document.getElementById("dpredChart").getContext('2d');
-                let pchart = new Chart(predChart, {
-                    type:'line',
-                    data:{
-                        labels: labels,
-                        datasets: [
-                            {
-                                label: "Potwierdzone przypadki",
-                                data: cases,
-                                backgroundColor: 'rgba(206, 2, 23, 0.6)',
-                                minBarLength: 100
-                            }
-                        ]
-                    },
-                    options:{
-                    }
-                });
-                let dpredCh = new Chart(dailypredChart, {
-                    type:'line',
-                    data:{
-                        labels: idkidk,
-                        datasets: [
-                            {
-                                label: "Dzienne przypadki",
-                                data: daily,
-                                backgroundColor: 'rgba(53, 133, 202, 0.6)',
-                                minBarLength: 100
-                            }
-                        ]
-                    },
-                    options:{
-                    }
-                });
-            });
+                }
+            })
+            .catch(error => console.log(error));
     } else {
         let y = document.querySelector('.huh');
         y.classList.remove('huh');
