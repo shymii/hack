3dccd7a462a33561e91c95db2949f52e85c2490f
raczu <61229912+raczu@users.noreply.github.com>
2020-10-25 18:40:00 +0100

    baza

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import fact
 
 # Register your models here.
+
+class fact_admin(admin.ModelAdmin):
+    model = fact
+    search_fields = ['title', 'text']
+    list_display = ['title', 'text']
+
+    fieldsets = [
+        ('Ciekawostka', {'fields': ['title', 'text']}),
+    ]
+
+admin.site.register(fact, fact_admin)
