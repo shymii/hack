09ffc4fac2a281f186d3242c9429c605b2a85a46
PCWZAREK\wzarek <katanaforumps2@gmail.com>
2020-10-25 17:38:48 +0100

    fixxx view

--- physical_health/views.py
+++ physical_health/views.py
@@ -7,15 +7,15 @@ from .decorators import is_not_session
 # Create your views here.
 @is_not_session
 def physical_health_view(request):
-    survey_result = user_survey.objects.filter(user = request.user.user_profile).order_by('-survey_date')[0]
-    if survey_result == None:
-        context = {}
-    else:
+    try:
+        survey_result = user_survey.objects.filter(user = request.user.user_profile).order_by('-survey_date')[0]
         year = request.user.user_profile.birth_date.year
         now = datetime.datetime.now()
         year_now = now.year
         birth_date = year_now - year
         gender = request.user.user_profile.sex
         context = {'survey_result' : survey_result, 'birth_date' : birth_date, 'gender' : gender}
+    except:
+        context = {}   
     template = 'physical_health/physical_health.html'
     return render(request, template, context)
